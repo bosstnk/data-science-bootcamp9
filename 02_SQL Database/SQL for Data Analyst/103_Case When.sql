b5ec0edbb1665fc821d60910108b90e9d3aedf80
bosstnk <167693130+bosstnk@users.noreply.github.com>
2024-07-30 19:25:13 +0700

    Create 103_Case When.sql

---
+++ 02_SQL Database/SQL for Data Analyst/103_Case When.sql
@@ -0,0 +1,22 @@
+-- CASE is like IF-ELSE in Google Sheets
+-- WHEN คือ เงื่อนไข (Condition)
+-- THEN คือ ผลลัพธ์ (Value)
+-- ELSE คือ เงื่อนไขอื่นๆ นอกเหนือจากที่กำหนด
+
+SELECT 
+	company,
+	CASE
+		WHEN company IS NOT NULL THEN 'Corporate'
+		ELSE 'End Customer'
+	END AS segment
+FROM customers;
+
+-- Case มากกว่า 1 เงื่อนไข
+SELECT 
+  country,
+  CASE
+    WHEN country IN ('Canada', 'USA') THEN 'America'
+    WHEN country IN ('Belgium', 'France', 'Italy') THEN 'Europe'
+    ELSE 'Other'
+  END AS region
+FROM customers
