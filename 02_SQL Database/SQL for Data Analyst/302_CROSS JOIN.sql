84d55a01b30f53d52af8376f6339b88263b4dd80
bosstnk <167693130+bosstnk@users.noreply.github.com>
2024-07-31 17:12:57 +0700

    Create 302_CROSS JOIN.sql

---
+++ 02_SQL Database/SQL for Data Analyst/302_CROSS JOIN.sql
@@ -0,0 +1,18 @@
+-- create two tables: ranks and suits (card deck)
+CREATE TABLE ranks (
+    rank TEXT NOT NULL
+);
+
+CREATE TABLE suits (
+    suit TEXT NOT NULL
+);
+
+INSERT INTO ranks(rank) 
+VALUES('2'),('3'),('4'),('5'),('6'),('7'),('8'),('9'),('10'),('J'),('Q'),('K'),('A');
+
+INSERT INTO suits(suit) 
+VALUES('Clubs'),('Diamonds'),('Hearts'),('Spades');
+
+-- cross join both tables to create a full card deck
+SELECT * FROM ranks 
+CROSS JOIN suits ORDER BY suit;
