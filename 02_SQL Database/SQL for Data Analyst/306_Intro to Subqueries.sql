96973dc9c223807dbedec608ca39ce5d177392e8
bosstnk <167693130+bosstnk@users.noreply.github.com>
2024-07-31 17:32:55 +0700

    Update 306_Intro to Subqueries.sql

--- 02_SQL Database/SQL for Data Analyst/306_Intro to Subqueries.sql
+++ 02_SQL Database/SQL for Data Analyst/306_Intro to Subqueries.sql
@@ -7,3 +7,24 @@ SELECT * from tracks WHERE milliseconds >= 5286953;
 SELECT * FROM tracks 
 WHERE milliseconds = (SELECT max(milliseconds) FROM tracks);
 
+---------------------------------------------------------
+
+--Count the number of songs that are longer than the average length of all the songs.
+--NOT subqueries in WHERE clause
+SELECT AVG(milliseconds) from tracks;
+SELECT COUNT(*) from tracks WHERE milliseconds >= 393599.2121039109;
+
+--subqueries in WHERE clause
+SELECT COUNT(*) FROM tracks 
+WHERE milliseconds >= (SELECT AVG(milliseconds) FROM tracks);
+
+---------------------------------------------------------
+
+--Show the first name, last name, and email of customers located in the USA.
+--NOT subqueries example in FROM clause
+SELECT firstname, lastname, email FROM customers
+WHERE country = 'USA'
+
+--subqueries example in FROM clause
+SELECT firstname, lastname, email
+FROM (SELECT * FROM customers WHERE country = 'USA');
